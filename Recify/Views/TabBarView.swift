a152194e9923a67781473d75a2b4e2477d727b7a
250evelyne <evelynekessie@gmail.com>
2026-03-09 13:07:01 -0400

    Merge Anabella's changes - CalendarView, RecipeInstructionsView, CookingModeTabView updates

--- Recify/Views/TabBarView.swift
+++ Recify/Views/TabBarView.swift
@@ -19,13 +19,28 @@ struct TabBarView: View {
                 }
                 .tag(0)
             
-            CookingModeTabView()
-                .tabItem {
-                    Image(systemName: selectedTab == 1 ? "play.circle.fill" : "play.circle")
-                    Text("Cooking")
-                }
-                .tag(1)
+            //gonna move this to user profile
+//            PantryView()
+//                .tabItem {
+//                    Image(systemName: selectedTab == 2 ? "archivebox.fill" : "archivebox")
+//                    Text("Pantry")
+//                }
+//                .tag(2)
             
+            //has to be somewhere else
+//            CookingModeTabView()
+//                .tabItem {
+//                    Image(systemName: selectedTab == 1 ? "play.circle.fill" : "play.circle")
+//                    Text("Cooking")
+//                }
+//                .tag(1)
+            
+            CommunityFeedView()
+                .tabItem {
+                    Image(systemName: selectedTab == 4 ? "person.3.fill" : "person.3")
+                    Text("Feed")
+                }
+                .tag(2)
             
             MessagesListView()
                 .tabItem {
@@ -34,12 +49,21 @@ struct TabBarView: View {
                 }
                 .tag(3)
             
+            //move this to home page
+//            ShoppingList()
+//                .tabItem {
+//                    Image(systemName: selectedTab == 4 ? "cart.fill" : "cart")
+//                    Text("Shop")
+//                }.tag(4)
+            
             ProfileView()
                 .tabItem {
                     Image(systemName: selectedTab == 4 ? "person.fill" : "person")
                     Text("Profile")
                 }
                 .tag(4)
+            
+            
         }
         .accentColor(.pink)
     }
