bb671016d520669618c6cf8df7d180637d3dc9f4
Alexanne <alexannefortin18@gmail.com>
2026-03-09 18:19:49 -0400

    chnage the shoping list view so that the ingredients of the same catgory would be under the same title and they dont have any the title vegtable twice, added the search results for the search and filters but idk what wrong with the search but it dosnt show me any thing

---
+++ Recify/Views/Search and advanced filters/SearchResults.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResults.swift
+//  Recify
+//
+//  Created by Macbook on 2026-03-08.
+//
+
+import SwiftUI
+
+struct SearchResults: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResults()
+}
---
+++ Recify/Views/Search and advanced filters/searchResultCard.swift
@@ -0,0 +1,18 @@
+//
+//  searchResultCard.swift
+//  Recify
+//
+//  Created by Macbook on 2026-03-08.
+//
+
+import SwiftUI
+
+struct searchResultCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    searchResultCard()
+}
